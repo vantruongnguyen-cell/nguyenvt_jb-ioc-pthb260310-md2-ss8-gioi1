8da4f07397c8e0fc65cf65489366e73c30afdd72
NguyenVT_JB-IOC-PTHB260310 <vantruongnguyen@thptchuyenlamson.edu.vn>
2026-05-11 11:28:56 +0700

    Add files via upload

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -32,22 +32,3 @@ public class CurrencyConverter {
         return String.format("$%.2f", usd);
     }
 }
-public class Main {
-    public static void main(String[] args) {
-        // 1. Hiển thị tỉ giá mặc định
-        System.out.println("Tỉ giá hiện tại: " + CurrencyConverter.getRate());
-
-        // 2. Chuyển đổi thử nghiệm
-        int myVnd = 500000;
-        double usd = CurrencyConverter.toUSD(myVnd);
-        System.out.println(myVnd + " VND quy đổi được: " + CurrencyConverter.formatUSD(usd));
-
-        // 3. Thay đổi tỉ giá và quy đổi lại
-        CurrencyConverter.setRate(23500.5);
-        double newUsd = CurrencyConverter.toUSD(myVnd);
-        System.out.println(myVnd + " VND với tỉ giá mới là: " + CurrencyConverter.formatUSD(newUsd));
-
-        // 4. Test validation
-        CurrencyConverter.setRate(-100);
-    }
-}
